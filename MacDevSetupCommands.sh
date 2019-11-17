3fadecbdb21c5cbb3f24525f57f179ffdff9fbab
Vincenzo Domina <54762917+vincenzodomina@users.noreply.github.com>
2019-11-17 17:00:22 +0100

    Create MacDevSetupCommands.sh

---
+++ MacDevSetupCommands.sh
@@ -0,0 +1,16 @@
+# Visualisation options
+
+# show path in finder
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+
+
+
+
+# Install following for web development
+
+# homebrew
+# gitflow
+# react
+# node.js
+# visual studio pro 
+# zsh
