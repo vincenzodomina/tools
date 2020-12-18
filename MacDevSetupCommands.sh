c9f256fa05f37e19703f27f7ba26536b648d55a4
Vincenzo Domina <54762917+vincenzodomina@users.noreply.github.com>
2020-12-18 10:48:31 +0100

    Update MacDevSetupCommands.sh

--- MacDevSetupCommands.sh
+++ MacDevSetupCommands.sh
@@ -1,4 +1,5 @@
-## Mac setup and apps
+### Mac development setup
+
 # show path in finder
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
 # brew cask install visual-studio-code
@@ -10,7 +11,8 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Find
 # Pretty Beautiful Javascript
 # JSON Formatter
 
-## Terminal
+### Terminal
+
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
@@ -24,10 +26,24 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 nvm install stable
 mkdir ~/Projects
 npm install -g lite-server eslint
+# Create github personal access token to use as git password: https://github.com/settings/tokens
+# Setup git with github: https://docs.github.com/en/free-pro-team@latest/github/getting-started-with-github/set-up-git
 brew install git
 brew install vcprompt
 
-## Frameworks for web development
+### Code editor
+
+# Visual Studio Code plugins:
+# Bracket Pair Colorizer 2
+# Tailwind CSS Intellisense
+# setting sync
+# SVG Viewer
+# path intellisense
+# sass
+# javascript code snippets
+# visual studio intellicode
+
+### Frameworks for web development
 
 # TailwindCSS
 npm init -y  #create package.json file
@@ -60,21 +76,12 @@ npm install @material-ui/lab # for e.g. rating with stars
 npm install material-table # tables https://material-table.com/
 
 
-# JavaScript
+### JavaScript
+
 npm install moment # managing time and dates in javascript
 
-## Visual Studio Code plugins:
-# Bracket Pair Colorizer 2
-# Tailwind CSS Intellisense
-# setting sync
-# SVG Viewer
-# path intellisense
-# sass
-# javascript code snippets
-# visual studio intellicode
 
-
-## Frameworks for native development
+### Frameworks for native development
 
 # React native
 brew install yarn
@@ -94,7 +101,8 @@ cd my-new-project
 expo start # then download Expo from Appstore on mobile phone and scan QR-code
 brew update && brew cask install react-native-debugger # RN dev tools
 
-## Databases
+### Databases
+
 # PostGreSQL Installer from https://www.enterprisedb.com/
 npm install pg # toolset for using node with postgres, API: const { Client } = require('pg')
 psql -d mydb -U myuser # Access database through terminal, \c (connect to db), \d mytable (list table content), \dt (list all tables), \q (quit connection)
@@ -113,6 +121,8 @@ npm install --save-dev nodemon # Save nodemon as dev dependency, start it with: 
 npm install axios # Make http requests from node.js
 npm install cors # cross-origin-ressource-sharing, providing a Connect/Express middleware that can be used to enable CORS 
 
+### Web services
+
 # Amazon Web Services
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"  # download package to current folder
 sudo installer -pkg ./AWSCLIV2.pkg -target /   #install
@@ -120,3 +130,16 @@ aws configure # configure iam user with access key and secret key and e.g. eu-ce
 aws s3 mb s3://your-bucket-name # creates a bucket
 aws s3 ls # lists buckets
 npm run build && aws s3 sync build/ s3://your-bucket-name  # builds and deploys the react app
+
+# Google cloud functions
+# Install gcloud command line tool: https://cloud.google.com/sdk/docs/install
+./google-cloud-sdk/install.sh # isntall paths
+./google-cloud-sdk/bin/gcloud init # initialize SDK and reopen terminal
+# Cloud functions: https://github.com/GoogleCloudPlatform/functions-framework-nodejs
+
+# Firebase
+npm install -g firebase-tools
+# Sign in to Google. in Firebase dashboard: Initiate a project
+firebase login
+firebase init
+firebase deploy
